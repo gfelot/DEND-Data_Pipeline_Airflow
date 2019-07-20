234608bcf95202537af8021a1c6f4f27d83e66a0
Gil <gil.felot@protonmail.com>
2019-07-20 21:45:56 +0200

    Using context manager for DAG + data_quality done

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -3,70 +3,38 @@ import os
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
-                                LoadDimensionOperator, DataQualityOperator)
+                               LoadDimensionOperator, DataQualityOperator)
 from helpers import SqlQueries
 
 # AWS_KEY = os.environ.get('AWS_KEY')
 # AWS_SECRET = os.environ.get('AWS_SECRET')
 
 default_args = {
-    'owner': 'udacity',
+    'owner': 'gil',
     'start_date': datetime(2019, 1, 12),
 }
 
-dag = DAG('udac_example_dag',
-          default_args=default_args,
-          description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *'
-        )
-
-start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
-
-stage_events_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_events',
-    dag=dag
-)
-
-stage_songs_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_songs',
-    dag=dag
-)
-
-load_songplays_table = LoadFactOperator(
-    task_id='Load_songplays_fact_table',
-    dag=dag
-)
-
-load_user_dimension_table = LoadDimensionOperator(
-    task_id='Load_user_dim_table',
-    dag=dag
-)
-
-load_song_dimension_table = LoadDimensionOperator(
-    task_id='Load_song_dim_table',
-    dag=dag
-)
-
-load_artist_dimension_table = LoadDimensionOperator(
-    task_id='Load_artist_dim_table',
-    dag=dag
-)
-
-load_time_dimension_table = LoadDimensionOperator(
-    task_id='Load_time_dim_table',
-    dag=dag
-)
-
-run_quality_checks = DataQualityOperator(
-    task_id='Run_data_quality_checks',
-    dag=dag
-)
-
-end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+# Using the context manager allows me not to duplicate the dag parameter in each operator
+with DAG('udac_example_dag',
+         default_args=default_args,
+         description='Load and transform data in Redshift with Airflow',
+         ) as dag:
+    start_operator = DummyOperator(task_id='Begin_execution')
+    stage_events_to_redshift = StageToRedshiftOperator(task_id='Stage_events')
+    stage_songs_to_redshift = StageToRedshiftOperator(task_id='Stage_songs')
+    load_songplays_table = LoadFactOperator(task_id='Load_songplays_fact_table')
+    load_user_dimension_table = LoadDimensionOperator(task_id='Load_user_dim_table')
+    load_song_dimension_table = LoadDimensionOperator(task_id='Load_song_dim_table')
+    load_artist_dimension_table = LoadDimensionOperator(task_id='Load_artist_dim_table')
+    load_time_dimension_table = LoadDimensionOperator(task_id='Load_time_dim_table')
+    run_quality_checks = DataQualityOperator(task_id='Run_data_quality_checks')
+    end_operator = DummyOperator(task_id='Stop_execution')
 
 # Make graph
 start_operator >> [stage_events_to_redshift, stage_songs_to_redshift]
 [stage_events_to_redshift, stage_songs_to_redshift] >> load_songplays_table
-load_songplays_table >> [load_artist_dimension_table, load_song_dimension_table, load_time_dimension_table, load_user_dimension_table]
-[load_artist_dimension_table, load_song_dimension_table, load_time_dimension_table, load_user_dimension_table] >> run_quality_checks
+load_songplays_table >> [load_artist_dimension_table, load_song_dimension_table, load_time_dimension_table,
+                         load_user_dimension_table]
+[load_artist_dimension_table, load_song_dimension_table, load_time_dimension_table,
+ load_user_dimension_table] >> run_quality_checks
 run_quality_checks >> end_operator
--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -8,15 +8,20 @@ class DataQualityOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 table_name="",
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table_name = table_name
 
-    def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+    def execute(self):
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+        records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {self.table_name}")
+        if len(records) < 1 or len(records[0]) < 1:
+            raise ValueError(f"Data quality check failed. {self.table_name} has no values")
+        num_records = records[0][0]
+        if num_records < 1:
+            raise ValueError(f"Data quality check failed. {self.table_name} contained 0 rows")
+        self.log.info(f"Data quality on table {self.table_name} check passed with {records[0][0]} records")
